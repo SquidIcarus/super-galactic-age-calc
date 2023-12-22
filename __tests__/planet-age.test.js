0c61c915f47fa5204bca509f2dc0042257910441
Landon Isaiah <landonisaiah@gmail.com>
2023-12-22 12:30:27 -0800

    log EarthHuman bad fail

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -3,8 +3,8 @@ import EarthHuman from '..src/js/planet-age.js';
 describe('EarthHuman', () => {
 
     test('should instantiate the human with name and birth year', () => {
-        const human = new EarthHuman("Zim", 1985);
-        expect(human.name).toBe("Zim");
-        expect(human.birthYear).toEqual(1985);
+        const earthling = new EarthHuman("Zim", 1985);
+        expect(earthling.humanName).toBe("Zim");
+        expect(earthling.earthlingBirthYear).toEqual(1985);
     })
 })
