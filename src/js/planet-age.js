1e602b2625a8b28b6fcc50758cc03f6f834ec096
Landon Isaiah <landonisaiah@gmail.com>
2023-12-22 12:55:41 -0800

    add logic to EarthHuman skeleton and log green pass

--- src/js/planet-age.js
+++ src/js/planet-age.js
@@ -1,2 +1,6 @@
 export default class EarthHuman {
+    constructor(humanName, earthlingBirthYear) {
+        this.humanName = humanName;
+        this.earthlingBirthYear = earthlingBirthYear;
+    }
 }
