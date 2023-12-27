08d27a7017fbbcdc4d64dd73a93bbcd1d271bdcd
Landon Isaiah <landonisaiah@gmail.com>
2023-12-26 17:07:13 -0800

    pass good fail for refactored EarthHuman and getEarthAge function

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -3,21 +3,21 @@ import EarthHuman from '../src/js/planet-age.js';
 describe('EarthHuman', () => {
 
     test('should instantiate the human with name and birth year', () => {
-        const earthling = new EarthHuman("Zim", 1985, 22);
+        const earthling = new EarthHuman("Zim", 2001, 22);
         expect(earthling.humanName).toBe("Zim");
-        expect(earthling.earthlingBirthYear).toEqual(1985);
+        expect(earthling.earthlingBirthYear).toEqual(2001);
         expect(earthling.earthAge).toEqual(22);
     })
-})
 
-describe('getEarthAge', () => {
+    describe('getEarthAge', () => {
 
-    test('should calculate earth age based on earthlingBirthYear', () => {
-        const currentYear = new Date().getFullYear();
-        const expectedAge = currentYear - 2001;
-        const earthling = new EarthHuman("Zim", 1985);
-        const earthAge = earthling.getEarthAge();
-        expect(earthAge).toEqual(expectedAge);
+        test('should calculate earth age based on earthlingBirthYear', () => {
+            const currentYear = new Date().getFullYear();
+            const expectedAge = currentYear - 2001;
+            const earthling = new EarthHuman("Zim", 2001);
+            const earthAge = earthling.getEarthAge();
+            expect(earthAge).toEqual(expectedAge);
+        })
     })
 });
 
--- src/js/planet-age.js
+++ src/js/planet-age.js
@@ -8,7 +8,4 @@ export default class EarthHuman {
     getEarthAge() {
 
     }
-
 }
-
-
