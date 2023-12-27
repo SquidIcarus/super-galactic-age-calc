df23c46e0c31300dbcb998b9d8b468024dd8a865
Landon Isaiah <landonisaiah@gmail.com>
2023-12-26 16:54:18 -0800

    refactor test for EarthHuman and getEarthAge

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -3,18 +3,21 @@ import EarthHuman from '../src/js/planet-age.js';
 describe('EarthHuman', () => {
 
     test('should instantiate the human with name and birth year', () => {
-        const earthling = new EarthHuman("Zim", 1985);
+        const earthling = new EarthHuman("Zim", 1985, 22);
         expect(earthling.humanName).toBe("Zim");
         expect(earthling.earthlingBirthYear).toEqual(1985);
+        expect(earthling.earthAge).toEqual(22);
     })
 })
 
 describe('getEarthAge', () => {
 
     test('should calculate earth age based on earthlingBirthYear', () => {
+        const currentYear = new Date().getFullYear();
+        const expectedAge = currentYear - 2001;
         const earthling = new EarthHuman("Zim", 1985);
-        earthling.getEarthAge();
-        expect(earthling.earthlingBirthYear).toEqual(22);
+        const earthAge = earthling.getEarthAge();
+        expect(earthAge).toEqual(expectedAge);
     })
 });
 
--- src/js/planet-age.js
+++ src/js/planet-age.js
@@ -2,6 +2,7 @@ export default class EarthHuman {
     constructor(humanName, earthlingBirthYear) {
         this.humanName = humanName;
         this.earthlingBirthYear = earthlingBirthYear;
+        this.earthAge = this.getEarthAge();
     }
 
     getEarthAge() {
