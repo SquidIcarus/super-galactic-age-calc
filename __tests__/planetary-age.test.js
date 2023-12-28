f9800bf37aea7f192a9367eab3cfb9c7d43c3207
Landon Isaiah <landonisaiah@gmail.com>
2023-12-27 22:21:20 -0800

    test venusAge for good fail

--- __tests__/planetary-age.test.js
+++ __tests__/planetary-age.test.js
@@ -1,11 +1,18 @@
-import { mercuryAge } from '../src/js/planetary-age.js';
+import { mercuryAge, venusAge } from '../src/js/planetary-age.js';
 import EarthHuman from '../src/js/earth-age.js';
 
 describe('planetAge', () => {
 
-    test('should calculate age on other planets based on EarthHuman earthAge', () => {
+    test('should calculate age on Mercury based on EarthHuman earthAge', () => {
         const earthlingAge = new EarthHuman("Zim", 2001);
         const mercuryAgeResult = mercuryAge(earthlingAge.getEarthAge());
-        expect(mercuryAgeResult).toEqual(91.67)
+        expect(mercuryAgeResult).toEqual(91.67);
     })
+
+    test('should calculate age on Venus based on EarthHuman earthAge', () => {
+        const earthlingAge = new EarthHuman("Zim", 2001);
+        const venusAgeResult = venusAge(earthlingAge.getEarthAge());
+        expect(venusAgeResult).toEqual(33.87);
+    })
+
 })
--- src/js/planetary-age.js
+++ src/js/planetary-age.js
@@ -1,3 +1,7 @@
 export function mercuryAge(earthAge) {
     return parseFloat((earthAge / 0.24).toFixed(2));
 }
+
+export function venusAge() {
+
+}
