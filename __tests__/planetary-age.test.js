4b9a1d9c1c069e7d1662749d486d04ac2c0e0ced
Landon Isaiah <landonisaiah@gmail.com>
2024-01-01 19:15:57 -0800

    refactor planetary-age.js and test to pass with updated Date function and the new year

--- __tests__/planetary-age.test.js
+++ __tests__/planetary-age.test.js
@@ -1,30 +1,30 @@
-// import { mercuryAge, venusAge, marsAge, jupiterAge } from '../src/js/planetary-age.js';
-// import EarthHuman from '../src/js/earth-age.js';
+import { mercuryAge, venusAge, marsAge, jupiterAge } from '../src/js/planetary-age.js';
+import EarthHuman from '../src/js/earth-age.js';
 
-// describe('planetAge', () => {
+describe('planetAge', () => {
 
-//     test('should calculate age on Mercury based on EarthHuman earthAge', () => {
-//         const earthlingAge = new EarthHuman("Zim", 2001);
-//         const mercuryAgeResult = mercuryAge(earthlingAge.getEarthAge());
-//         expect(mercuryAgeResult).toEqual(91.67);
-//     })
+    test('should calculate age on Mercury based on EarthHuman earthAge', () => {
+        const earthlingAge = new EarthHuman("Zim", 2001);
+        const mercuryAgeResult = mercuryAge(earthlingAge.getEarthAge());
+        expect(mercuryAgeResult).toEqual(95.83);
+    })
 
-//     test('should calculate age on Venus based on EarthHuman earthAge', () => {
-//         const earthlingAge = new EarthHuman("Zim", 2001);
-//         const venusAgeResult = venusAge(earthlingAge.getEarthAge());
-//         expect(venusAgeResult).toEqual(35.48);
-//     })
+    test('should calculate age on Venus based on EarthHuman earthAge', () => {
+        const earthlingAge = new EarthHuman("Zim", 2001);
+        const venusAgeResult = venusAge(earthlingAge.getEarthAge());
+        expect(venusAgeResult).toEqual(37.10);
+    })
 
-//     test('should calculate age on Mars based on EarthHuman earthAge', () => {
-//         const earthlingAge = new EarthHuman("Zim", 2001);
-//         const marsAgeResult = marsAge(earthlingAge.getEarthAge());
-//         expect(marsAgeResult).toEqual(11.70);
-//     })
+    test('should calculate age on Mars based on EarthHuman earthAge', () => {
+        const earthlingAge = new EarthHuman("Zim", 2001);
+        const marsAgeResult = marsAge(earthlingAge.getEarthAge());
+        expect(marsAgeResult).toEqual(12.23);
+    })
 
-//     test('should calculate age on Jupiter based on EarthHuman earthAge', () => {
-//         const earthlingAge = new EarthHuman("Zim", 2001);
-//         const jupiterAgeResult = jupiterAge(earthlingAge.getEarthAge());
-//         expect(jupiterAgeResult).toEqual(1.85);
-//     })
+    test('should calculate age on Jupiter based on EarthHuman earthAge', () => {
+        const earthlingAge = new EarthHuman("Zim", 2001);
+        const jupiterAgeResult = jupiterAge(earthlingAge.getEarthAge());
+        expect(jupiterAgeResult).toEqual(1.94);
+    })
 
-// })
+})
