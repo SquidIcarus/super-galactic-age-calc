2aa2fd5e63569e94d226b867503f91dd80ddd03f
Landon Isaiah <landonisaiah@gmail.com>
2024-01-01 21:31:15 -0800

    green pass years-till.test.js

--- __tests__/years-till.test.js
+++ __tests__/years-till.test.js
@@ -1,35 +1,36 @@
 import { earthYearsTill, mercuryYearsTill, venusYearsTill, marsYearsTill, jupiterYearsTill } from '../src/js/years-till.js';
 
 describe('yearsTill', () => {
-    let currentAge, futureAge;
+    let futureAge, currentAge;
 
     beforeEach(() => {
-        currentAge = 23;
         futureAge = 50;
+        currentAge = 23;
+
     });
 
     test('should calculate years till next birth year on Earth based on chosen futureAge', () => {
-        const result = earthYearsTill(currentAge, futureAge);
+        const result = earthYearsTill(futureAge, currentAge);
         expect(result).toEqual(27);
     });
 
     test('should calculate years till next birth year on Mercury based on chosen futureAge', () => {
-        const result = mercuryYearsTill(currentAge, futureAge);
+        const result = mercuryYearsTill(futureAge, currentAge);
         expect(result).toEqual(112.5);
     });
 
     test('should calculate years till next birth year on Venus based on chosen futureAge', () => {
-        const result = venusYearsTill(currentAge, futureAge);
+        const result = venusYearsTill(futureAge, currentAge);
         expect(result).toEqual(43.55);
     });
 
     test('should calculate years till next birth year on Mars based on chosen futureAge', () => {
-        const result = marsYearsTill(currentAge, futureAge);
+        const result = marsYearsTill(futureAge, currentAge);
         expect(result).toEqual(14.36);
     });
 
     test('should calculate years till next birth year on Jupiter based on chosen futureAge', () => {
-        const result = jupiterYearsTill(currentAge, futureAge);
+        const result = jupiterYearsTill(futureAge, currentAge);
         expect(result).toEqual(2.28);
     });
 });
--- src/js/years-till.js
+++ src/js/years-till.js
@@ -1,22 +1,25 @@
-export function earthYearsTill() {
+export function earthYearsTill(futureAge, currentAge) {
+    return futureAge - currentAge;
+}
+
+export function mercuryYearsTill(futureAge, currentAge) {
+    const earthYrsTill = earthYearsTill(futureAge, currentAge);
+    return parseFloat((earthYrsTill / 0.24).toFixed(2));
+}
+
+export function venusYearsTill(futureAge, currentAge) {
+    const earthYrsTill = earthYearsTill(futureAge, currentAge);
+    return parseFloat((earthYrsTill / 0.62).toFixed(2));
+}
+
+export function marsYearsTill(futureAge, currentAge) {
+    const earthYrsTill = earthYearsTill(futureAge, currentAge);
+    return parseFloat((earthYrsTill / 1.88).toFixed(2));
 
 }
 
-export function mercuryYearsTill() {
-
-
-}
-
-export function venusYearsTill() {
-
-}
-
-export function marsYearsTill() {
-
-
-}
-
-export function jupiterYearsTill() {
-
+export function jupiterYearsTill(futureAge, currentAge) {
+    const earthYrsTill = earthYearsTill(futureAge, currentAge);
+    return parseFloat((earthYrsTill / 11.86).toFixed(2));
 
 }
