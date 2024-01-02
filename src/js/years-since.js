c25ec59ca3fdfaaefc97f7c67de2beb5010c8014
Landon Isaiah <landonisaiah@gmail.com>
2024-01-01 20:43:13 -0800

    green pass venus, mars and jupiterYearsSince functions

--- src/js/years-since.js
+++ src/js/years-since.js
@@ -9,13 +9,18 @@ export function mercuryYearsSince(currentAge, pastAge) {
 }
 
 export function venusYearsSince(currentAge, pastAge) {
-
+    const earthYrsPassed = earthYearsSince(currentAge, pastAge);
+    return parseFloat((earthYrsPassed / 0.62).toFixed(2));
 }
 
 export function marsYearsSince(currentAge, pastAge) {
+    const earthYrsPassed = earthYearsSince(currentAge, pastAge);
+    return parseFloat((earthYrsPassed / 1.88).toFixed(2));
 
 }
 
 export function jupiterYearsSince(currentAge, pastAge) {
+    const earthYrsPassed = earthYearsSince(currentAge, pastAge);
+    return parseFloat((earthYrsPassed / 11.86).toFixed(2));
 
 }
